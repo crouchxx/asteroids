196092d13a2056e88d29233ee9167b042549bdc0
crouchxx <hanusekpatrik@gmail.com>
2025-11-17 15:09:27 +0100

    Added: player controls

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2) 
 
     while 1:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
-        pygame.display.flip(screen)
-    #print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+    
+
 
 
 if __name__ == "__main__":
